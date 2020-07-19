57dffe2cb936393a998aa1bb9618621bc6ef9c1c
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-07-19 13:39:36 +0700

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+export const SEO = {
+  title: 'UploadImg',
+  description: 'Upload and explore images',
+  keywords: 'upload image, image hosting',
+  openGraph: {
+    type: 'website',
+    url: 'https://uploadimg.vercel.app/',
+    title: 'UploadImg',
+    description: 'Upload and explore images',
+    image: 'https://uploadimg.vercel.app/ogimg.png',
+  },
+}
+
+export const GITHUB_URL = 'https://github.com/phumoonlight/web-uploadimg'
