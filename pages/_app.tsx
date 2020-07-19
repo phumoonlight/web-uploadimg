0ed1d5885607ce7e0faa7defb01d875afe0ff126
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-07-19 11:55:26 +0700

    fixed meta og

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,11 +12,11 @@ const App = ({ Component, pageProps }: TAppProps) => (
       <meta name="keywords" content="upload image, image hosting" />
       <meta name="author" content="Poosarn Moolmuang" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:url" content="https://uploadimg.now.sh" />
+      <meta property="og:url" content="https://uploadimg.vercel.app/" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="UploadImg" />
       <meta property="og:description" content="Upload and explore images" />
-      <meta property="og:image" content="https://uploadimg.now.sh/ogimg.png" />
+      <meta property="og:image" content="https://uploadimg.vercel.app/ogimg.png" />
     </Head>
     <Component {...pageProps} />
   </>
