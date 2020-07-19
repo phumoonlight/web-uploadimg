faba48b106090b8db4ed4f73762e99daca3f4193
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-07-19 13:39:49 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import { AppProps as TAppProps } from 'next/app'
 import Head from 'next/head'
+import { SEO } from '../src/config'
 import '../src/global.css'
 
 const App = ({ Component, pageProps }: TAppProps) => (
   <>
     <Head>
-      <title>UploadImg</title>
+      <title>{SEO.title}</title>
       <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-      <meta name="description" content="Upload and explore images" />
-      <meta name="keywords" content="upload image, image hosting" />
-      <meta name="author" content="Poosarn Moolmuang" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:url" content="https://uploadimg.vercel.app/" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="UploadImg" />
-      <meta property="og:description" content="Upload and explore images" />
-      <meta property="og:image" content="https://uploadimg.vercel.app/ogimg.png" />
+      <meta name="description" content={SEO.description} />
+      <meta name="keywords" content={SEO.keywords} />
+      <meta property="og:url" content={SEO.openGraph.url} />
+      <meta property="og:type" content={SEO.openGraph.type} />
+      <meta property="og:title" content={SEO.openGraph.title} />
+      <meta property="og:description" content={SEO.openGraph.description} />
+      <meta property="og:image" content={SEO.openGraph.image} />
     </Head>
     <Component {...pageProps} />
   </>
