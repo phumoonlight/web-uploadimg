df4a7f36c16d2504e37b87ae8f8d1d101951b93b
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-04 14:33:29 +0700

    Update index.tsx
    - change top label
    - add link to images

--- src/components/ImgExplore/index.tsx
+++ src/components/ImgExplore/index.tsx
@@ -5,16 +5,18 @@ interface ImgExploreProps {
   imgUrls: string[]
 }
 
-const ImgExplore: React.FunctionComponent<ImgExploreProps> = (props) => {
+const ImgExplore: React.FunctionComponent<ImgExploreProps> = (props: ImgExploreProps) => {
   const { imgUrls } = props
   return (
     <div className={css.root}>
       <div className={css.divider}>
-        -- EXPLORE MORE IMAGES --
+        -- MORE UPLOADED IMAGES --
       </div>
       {imgUrls.map((imgUrl) => (
-        <div className={css.imgcontainer}>
-          <img key={imgUrl} src={imgUrl} alt="public-img" />
+        <div key={imgUrl} className={css.imgcontainer}>
+          <a href={imgUrl} target="_blank" rel="noopener noreferrer">
+            <img src={imgUrl} alt="public-img" />
+          </a>
         </div>
       ))}
     </div>
