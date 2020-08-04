0d9148bcd40a6dabab3acbab24b4318e390c2ef1
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-04 19:47:35 +0700

    remove title description

--- src/components/AppTitle.tsx
+++ src/components/AppTitle.tsx
@@ -5,7 +5,6 @@ import css from './AppTitle.module.css'
 const AppTitle: React.FunctionComponent = (): JSX.Element => (
   <div className={css.root}>
     <h1>{APP.title}</h1>
-    <h3>{APP.description}</h3>
   </div>
 )
 
