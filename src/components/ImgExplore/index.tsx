7b13097a7efaaa05df7124cb21d7f653b27f53b8
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-04 20:19:24 +0700

    display partial of images, add load more image button

--- src/components/ImgExplore/index.tsx
+++ src/components/ImgExplore/index.tsx
@@ -7,18 +7,29 @@ interface ImgExploreProps {
 
 const ImgExplore: React.FunctionComponent<ImgExploreProps> = (props: ImgExploreProps) => {
   const { imgUrls } = props
+  const [maxDisplayImage, setMaxDisplayImage] = React.useState(10)
+  const partialImageURLs = imgUrls.slice(0, maxDisplayImage)
+  const handleClickMoreImage = () => setMaxDisplayImage(maxDisplayImage + 10)
   return (
     <div className={css.root}>
-      <div className={css.divider}>
-        -- MORE UPLOADED IMAGES --
-      </div>
-      {imgUrls.map((imgUrl) => (
+      <div className={css.divider}>RECENT UPLOADED IMAGES</div>
+      {partialImageURLs.map((imgUrl) => (
         <div key={imgUrl} className={css.imgcontainer}>
           <a href={imgUrl} target="_blank" rel="noopener noreferrer">
             <img src={imgUrl} alt="public-img" />
           </a>
         </div>
       ))}
+      {maxDisplayImage <= partialImageURLs.length && (
+        <div>
+          <input
+            className={css.buttonloadimg}
+            type="button"
+            value="Load more images"
+            onClick={handleClickMoreImage}
+          />
+        </div>
+      )}
     </div>
   )
 }
