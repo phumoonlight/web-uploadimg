2d477fd3256c1a83c47c515b0b2862fe311baa97
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-04 13:59:17 +0700

    refactor

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,13 @@ class MyDocument extends Document {
         <Head />
         <body>
           <div id="fb-root" />
-          <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&autoLogAppEvents=1&version=v7.0&appId=3123211517799491" nonce="DDTMtXeA" />
+          <script
+            async
+            defer
+            crossOrigin="anonymous"
+            src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&autoLogAppEvents=1&version=v7.0&appId=3123211517799491"
+            nonce="DDTMtXeA"
+          />
           <Main />
           <NextScript />
         </body>
